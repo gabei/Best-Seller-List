5ae611b5987d825aba9ee43ac19b916f91eb2934
gabei <gabe1792@gmail.com>
2023-05-16 23:23:51 -0500

    prints NYT best seller lists. no descriptions added yet

--- API/call-api.py
+++ API/call-api.py
@@ -3,12 +3,25 @@ import os
 import requests
 
 load_dotenv()
-NYTBestSellersUrlAndApiKey = os.getenv("BOOKS_API_FULL_URL")
+NYT_bestsellers_fiction_API = os.getenv("BOOKS_API_FICTION")
+NYT_bestsellers_nonfiction_API = os.getenv("BOOKS_API_NONFICTION")
 
-# API URL
-# https://api.nytimes.com/svc/books/v3/lists/best-sellers
-r = requests.get(NYTBestSellersUrlAndApiKey)
-bestSellersList = r.json()['results']['books']
 
-for book in bestSellersList:
-    print(book['title'], 'by', book['author'])
+def get_list(API_URL):
+    r = requests.get(API_URL)
+    the_list = r.json()['results']['books']
+
+    print('Rank\tTitle\tAuthor\tWeeks on List')
+
+    for book in the_list:
+        title = book['title']
+        author = book['author']
+        rank = book['rank']
+        weeks_on_list = book['weeks_on_list']
+
+        print(f'{rank}\t{title}\t\t\t\t{author}\t{weeks_on_list}')
+    print("\n")
+
+
+get_list(NYT_bestsellers_fiction_API)
+get_list(NYT_bestsellers_nonfiction_API)
