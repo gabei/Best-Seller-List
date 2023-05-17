3fb12d8d22b71970a210ca44fff165917ded90ac
gabei <admin@LTLIBRARY.ORG>
2023-05-17 18:05:17 -0500

    adds begginings of default table setup, not using docbuilder

--- DOC/build-document.py
+++ DOC/build-document.py
@@ -1,1 +1,11 @@
 from docx import Document
+
+
+doc = Document()
+
+# Heading 
+doc.add_heading("The New York Times Best Seller List")
+doc.add_page_break()
+
+# Table
+doc.add_table(rows=16, cols=4)
