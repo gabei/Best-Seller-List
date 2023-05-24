8960f382bd7b88f764fc5b9f1f8c9d6bf10534f5
gabei <admin@LTLIBRARY.ORG>
2023-05-24 17:15:59 -0500

    spaced out docstrings a bit for readability

--- app/doc_builder.py
+++ app/doc_builder.py
@@ -12,6 +12,7 @@ class DocBuilder:
     def define_margins_inches(self, margin_list: list) -> list:
         """
         Sets the document margins in clock-wise order from margin_list, starting from left and ending on bottom.
+
         Expects: A list of four page margins in inches.
         Returns: The margin list if successful.
         """
@@ -31,9 +32,11 @@ class DocBuilder:
     def set_document_default_font_and_size(self, font_name="Arial", font_size="10"):
         """
         Sets the default font and font size for the document.
+
         Expects: Font name as a string, font size as an int.
         Returns: Tuple containing both values if successful.
         """
+
         style = self.__doc.styles['Normal']
         font = style.font
         font.name = font_name
@@ -43,6 +46,7 @@ class DocBuilder:
 
     def create_header(self, text):
         """Builds a document header using the passed text.
+
         - Expects a string
         - Returns the text if successful.
         """
@@ -59,6 +63,7 @@ class DocBuilder:
     def create_table(self, r: int, c: int):
         """
         Creates a table of specified rows and columns. Creates a blank table if not specified.
+
         - Expects integers > 0 for rows and cols
         - Invalid input will create a blank table
         - Returns 1 if successful
@@ -70,9 +75,11 @@ class DocBuilder:
     def create_headers_for_table(self, header_list: list) -> None:
         """
         Sets the text of the header cells of the table in header_list order: left -> right
+
         Expects: A list of strings
         Returns: The list if successful
         """
+
         # Define the header cells of the table
         headers = self.table.rows[0].cells
 
@@ -84,10 +91,10 @@ class DocBuilder:
         # Apply List to Headers
         for cell, value in enumerate(header_list):
             headers[cell].text = value
-            # Center the text
+            # Center the text TODO: styling methods
             header_text = headers[cell].paragraphs[0]
             header_text.alignment = WD_TABLE_ALIGNMENT.CENTER
-            # Bold Text
+            # Bold Text TODO: styling methods
             headers[cell].paragraphs[0].runs[0].font.bold = True
 
         return header_list
@@ -95,9 +102,11 @@ class DocBuilder:
     def set_column_widths_inches(self, width_list: list) -> list:
         """
         Sets table column widths in order of those supplied by width_list.
+
         Expects: List of widths in INCHES
         Returns: The list of lengths if successful.
         """
+
         cols = self.table.columns
 
         if not self.table: 
@@ -117,6 +126,7 @@ class DocBuilder:
     def build_row_from(self, data):
         """
         Builds a row of table data using the book info passed.
+        
         Expects an object
             - NYT Best Seller Data:
             - Rank, title, author, description, Rank last week, Weeks on list
