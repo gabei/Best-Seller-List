238321fdaeb436124dd9b324748bab81fe077515
gabei <admin@LTLIBRARY.ORG>
2023-05-25 18:25:01 -0500

    Adjusted the filename generator

--- app/setup.py
+++ app/setup.py
@@ -5,14 +5,12 @@ import call_api as API_CALLER
 import stylizer
 
 
-
 load_dotenv()
 NYT_bestsellers_fiction_API = os.getenv("BOOKS_API_FICTION")
 NYT_bestsellers_nonfiction_API = os.getenv("BOOKS_API_NONFICTION")
 
 fiction_book_list = API_CALLER.create_book_list(NYT_bestsellers_fiction_API)
 nonfiction_book_list = API_CALLER.create_book_list(NYT_bestsellers_nonfiction_API)
-
 
 # Build the Word Document
 doc = docbuilder.DocBuilder()
@@ -44,4 +42,4 @@ column_widths_inches = [0.5, 7, 0.6, 0.7]
 doc.set_column_widths_inches(column_widths_inches)
 
 # Save our work!
-doc.save_document('test')
+doc.save_document(f'NYT-BS-List_{fiction_book_list["list_name"]}_{fiction_book_list["list_date"]}')
